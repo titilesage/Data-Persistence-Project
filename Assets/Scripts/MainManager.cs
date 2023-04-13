5aab8044075cda2487ade7a0175f333dcd2a3051
titilesage <122631795+titilesage@users.noreply.github.com>
2023-04-13 16:21:16 +0200

    cmmit 2
    final

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -11,21 +11,22 @@ public class MainManager : MonoBehaviour
     public Rigidbody Ball;
 
     public Text ScoreText;
+    public Text BestScoreText;
     public GameObject GameOverText;
-    
+
     private bool m_Started = false;
     private int m_Points;
-    
+
     private bool m_GameOver = false;
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
         for (int i = 0; i < LineCount; ++i)
         {
             for (int x = 0; x < perLine; ++x)
@@ -36,6 +37,8 @@ public class MainManager : MonoBehaviour
                 brick.onDestroyed.AddListener(AddPoint);
             }
         }
+
+        BestScoreText.text = "Best Score "+PlayerPrefs.GetString("PlayerName")+ " : " + PlayerPrefs.GetInt("BestScore", 0);
     }
 
     private void Update()
@@ -66,12 +69,20 @@ public class MainManager : MonoBehaviour
     {
         m_Points += point;
         ScoreText.text = $"Score : {m_Points}";
+
     }
 
     public void GameOver()
     {
         m_GameOver = true;
         GameOverText.SetActive(true);
-        SaveManager.Instance.score += m_Points;
+
+        int bestScore = PlayerPrefs.GetInt("BestScore", 0);
+        if (m_Points > bestScore)
+        {
+            bestScore = m_Points;
+            PlayerPrefs.SetInt("BestScore", bestScore);
+            BestScoreText.text = "Best Score: " + bestScore;
+        }
     }
 }
--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,14 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEditor;
 using UnityEngine.SceneManagement;
+using System.Net.Sockets;
+using System.Net;
+using System.Security.Cryptography;
+using System.Xml;
+
+[System.Serializable]
+public struct PlayerScore
+{
+    public string playerName;
+    public int score;
+}
 
 public class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TMP_InputField playerNameInput;
+    public Text bestScoreText;
+    public Text playerNameText;
+    public Text topScoresText;
+
+    private string playerName;
+    private int bestScore;
+    private List<PlayerScore> playerScores = new List<PlayerScore>();
+
+    private void Start()
     {
-        
+        bestScore = PlayerPrefs.GetInt("BestScore", 0);
+        playerName = PlayerPrefs.GetString("PlayerName", "Player");
+        playerNameInput.text = playerName;
+        bestScoreText.text = "Best Score: " + bestScore;
+        playerNameText.text = "Player Name: " + playerName;
+
+        LoadScores();
+        UpdateTopScores();
+    }
+
+    public void SaveData()
+    {
+        playerName = playerNameInput.text;
+        PlayerPrefs.SetString("PlayerName", playerName);
+        playerNameText.text = "Player Name: " + playerName;
+        AddScore(playerName, bestScore);
+    }
+
+    public void LoadData()
+    {
+        bestScore = PlayerPrefs.GetInt("BestScore", 0);
+        playerName = PlayerPrefs.GetString("PlayerName", "Player");
+        playerNameInput.text = playerName;
+        bestScoreText.text = "Best Score: " + bestScore;
+        playerNameText.text = "Player Name: " + playerName;
+        LoadScores();
+        UpdateTopScores();
     }
 
     public void LanceJeu()
@@ -18,10 +66,79 @@ public class Menu : MonoBehaviour
 
     public void SortJeu()
     {
+        SaveManager.Instance.SaveDatas();
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
         Application.Quit();
+#endif
     }
-    void Update()
+
+    private void AddScore(string playerName, int score)
     {
-        
+        bool scoreAdded = false;
+        for (int i = 0; i < playerScores.Count; i++)
+        {
+            if (score > playerScores[i].score)
+            {
+                playerScores.Insert(i, new PlayerScore { playerName = playerName, score = score });
+                scoreAdded = true;
+                break;
+            }
+        }
+
+        if (!scoreAdded)
+        {
+            playerScores.Add(new PlayerScore { playerName = playerName, score = score });
+        }
+
+        while (playerScores.Count > 5)
+        {
+            playerScores.RemoveAt(playerScores.Count - 1);
+        }
+
+        SaveScores();
+        UpdateTopScores();
+    }
+
+    private void SaveScores()
+    {
+        PlayerPrefs.SetInt("BestScore", playerScores[0].score);
+        PlayerPrefs.SetString("BestScorePlayerName", playerScores[0].playerName);
+        for (int i = 1; i < playerScores.Count; i++)
+        {
+            PlayerPrefs.SetInt("TopScore" + i, playerScores[i].score);
+            PlayerPrefs.SetString("TopScorePlayerName" + i, playerScores[i].playerName);
+        }
+    }
+
+    private void LoadScores()
+    {
+        playerScores.Clear();
+        playerScores.Add(new PlayerScore
+        {
+            playerName = PlayerPrefs.GetString("BestScorePlayerName", "Player"),
+            score = PlayerPrefs.GetInt("BestScore", 0)
+        });
+        for (int i = 1; i < 5; i++)
+        {
+            if (PlayerPrefs.HasKey("TopScore" + i))
+            {
+                playerScores.Add(new PlayerScore
+                {
+                    playerName = PlayerPrefs.GetString("TopScorePlayerName" + i, "Player"),
+                    score = PlayerPrefs.GetInt("TopScore" + i, 0)
+                });
+            }
+        }
+        UpdateTopScores();
+    }
+    private void UpdateTopScores()
+    {
+        topScoresText.text = "";
+        for (int i = 0; i < playerScores.Count; i++)
+        {
+            topScoresText.text += (i + 1) + ". " + playerScores[i].playerName + ": " + playerScores[i].score + "\n";
+        }
     }
 }
